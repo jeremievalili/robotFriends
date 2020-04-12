ed6c249acaf8833eefb28f1255e608cbcdd903ac
Jeremie Valili <56654756+jeremievalili@users.noreply.github.com>
2020-04-12 19:09:19 +0200

    First version of the app RoboFriends with React

---
+++ precache-manifest.250f2cfbb63e58dafb115eb4a0a879db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1ee22f4fe9f885bbcd6f4c5bd21896a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1667e5f66b9356d7ea10",
+    "url": "/static/css/main.b2d13c32.chunk.css"
+  },
+  {
+    "revision": "b09fc992b9b017fee532",
+    "url": "/static/js/2.0bc7f1f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0bc7f1f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1667e5f66b9356d7ea10",
+    "url": "/static/js/main.bb5258f7.chunk.js"
+  },
+  {
+    "revision": "700cce3a7caf4f992d6c",
+    "url": "/static/js/runtime-main.ebe91ffd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
