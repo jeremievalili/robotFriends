502c2b1f7649fe637382dff986f057a42507ced6
Jeremie Madara <jeremiemadara@gmail.com>
2020-04-20 17:05:09 +0200

    Update my app
    I made a mistake with my first commit...

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+ {
+     id: 1,
+     name: 'JereBot Valili',
+     username: 'JereBot',
+     email: 'Jeremie@april.biz',
+ },
+ {
+    id: 2,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz',
+},
+{
+    id: 3,
+    name: 'Nicholas Runolf',
+    username: 'Maxime_Nienow',
+    email: 'sherwood@rosamond.me',
+},
+{
+    id: 4, 
+    name: 'Charles Deen',
+    username: 'Charles Deen',
+    email: 'charlesdeen91@gmail.com',
+},
+{
+    id: 5,
+    name: 'Howard Madara',
+    username: 'Howard Le Brave',
+    email: 'Howardmadara@gmail.com'
+},
+{
+    id: 6,
+    name: 'Gustave Flaubert',
+    username: 'Gusgus Flow',
+    email: 'gusgus29@hot.com'
+},
+{
+    id: 7,
+    name: 'LynBot NanRot',
+    username: 'LyNan Bot',
+    email: 'cnm.superstar@queen.pa'
+},
+{
+    id: 8,
+    name: 'Yess JunJun',
+    username: 'Yess Le Conquerant',
+    email: 'yessvousaime@yemail.oui'
+},
+{
+    id: 9,
+    name: 'Alicia Bot',
+    username: 'The Queen Bot',
+    email: 'aliciarobot@gmail.al'
+},
+{
+    id: 10,
+    name: 'Corretta Bot',
+    username: 'Robot Musical',
+    email: 'Coco-music-bot@cucina.co'
+},
+]
