3f787fa68546278eec219b723a8c52f60c013afd
Jeremie Madara <jeremiemadara@gmail.com>
2020-04-20 18:07:08 +0200

    Add the ErrorBoundry

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErroBoundry';
 import './App.css';
 
 
@@ -40,7 +41,9 @@ class App extends Component {
                     <SearchBox searchChange= {this.onSearchChange}/>
 
                     <Scroll> 
-                    <CardList robots= { filteredRobots } />
+                        <ErrorBoundry>
+                            <CardList robots= { filteredRobots } />
+                        </ErrorBoundry>
                     </Scroll>
                 </div>
                 );  
